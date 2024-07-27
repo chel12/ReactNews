4dee98c89d1cc4251731bab8dc7aa31757feaf25
Semyon <sema.chel12@gmail.com>
2024-07-27 16:38:04 +0300

    ReduxToolkit RTKQuery

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Main from './pages/Main/Main';
 import { useTheme } from './context/ThemeContext';
 function App() {
 	const { isDark } = useTheme();
+
 	return (
 		<div className={`app ${isDark ? 'dark' : 'light'}`}>
 			<Header />
--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -1,13 +1,9 @@
-import { useFetch } from '../../helpers/hooks/useFetch';
 import styles from './styles.module.css';
 import BannersList from '../BannersList/BannersList';
-import { getLatestNews } from '../../api/apiNews';
-import { NewsApiResponse } from '../../interfaces';
-
-
+import { useGetLatestNewsQuery } from '../../store/services/newsApi';
 
 const LatestNews = () => {
-	const { data, isLoading } = useFetch<NewsApiResponse, null>(getLatestNews);
+	const { data, isLoading } = useGetLatestNewsQuery(null);
 	return (
 		<section className={styles.section}>
 			<BannersList banners={data && data.news} isLoading={isLoading} />
